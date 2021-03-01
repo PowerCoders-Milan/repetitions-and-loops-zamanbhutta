62cb82dea7774a91fd53212d0e9e01f7966d4b12
zamanbhutta <marham.ayat@gmail.com>
2021-03-01 15:33:30 +0100

    done 3

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,16 @@
 // write your code below
+//for (var number =0 ; number <= 10 ; number++) {
+    //var result = number * 9;
+  //   console.log(`${number} * 9 = ${result}`);
+//}
+ 
+
+
+
+//optional
+
+ var number = prompt('Enter a number: ');
+for(var i = 1; i <= 10; i++) {
+    var result = i * number;
+      console.log(`${number} * ${i} = ${result}`);
+   }
