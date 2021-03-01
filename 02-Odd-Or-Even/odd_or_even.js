4ff53d3e6c0d48f2ea40614db32fe93b4d1757aa
zamanbhutta <marham.ayat@gmail.com>
2021-03-01 14:35:43 +0100

    done 2

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,3 +5,33 @@
   // check if the current is even or odd
 
   // report the result to the screen
+
+  for (var num = 0 ; num <= 20 ; num++) {
+    if (num % 2 == 0){
+    console.log( `the number ${num} is even`); 
+  }
+  else { console.log(`the number ${num} is even`);
+}
+  }
+
+// while loop
+var i = 0;
+while (i <=20){
+  if (i % 2 == 0){
+    console.log(`the number ${i} is even`);
+  
+  }else {
+    console.log(`the number ${i} is even`);
+  }
+  i ++;
+}
+// do while loop
+var no = 0;
+do {
+  if (no % 2 == 0){
+    console.log(`the number ${no} is even`);
+  }else {
+    console.log(`the number ${no} is even`);
+}
+no++
+} while (no <= 20);
